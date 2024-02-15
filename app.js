ecd998ac90f992cfa18b27102959036478488edc
tibetdev <tibetdev@gmail.com>
2024-02-15 21:09:31 +0100

    cors header is added

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 
 import usersRouter from './routes/users.js';
 import messagesRouter from './routes/messages.js';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/users', usersRouter);
